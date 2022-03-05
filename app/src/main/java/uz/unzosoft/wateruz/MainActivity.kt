fa0a1db9d2df0c47e047a3baa50a2d03bd94defa
AbdurashidovShahzod <shahzod9933@gmail.com>
2022-03-05 21:53:28 +0500

    Initial commit

---
+++ app/src/main/java/uz/unzosoft/wateruz/MainActivity.kt
@@ -0,0 +1,17 @@
+package uz.unzosoft.wateruz
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import uz.unzosoft.wateruz.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+
+    }
+}
